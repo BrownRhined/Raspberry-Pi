bee3879e9526339f8c02a3100dce847148b8944d
BrownRhined <a728728728@gmail.com>
2020-04-05 04:12:44 +0800

    add

--- capturePhoto.sh
+++ capturePhoto.sh
@@ -11,5 +11,6 @@ mkdir -p /home/pi/eoa-ai-image/$FolderName
 # 將banner設定成透明，並加入時間戳記與調整字型
 fswebcam -r 1280x720 -S 20 --banner-colour '#FF000000' --line-colour '#FF000000' --timestamp '%Y-%m-%d %H:%M' --font 'sans:32' /home/pi/eoa-ai-image/$FolderName/$DATE.jpg
 
+
 # return 0 to system
 exit 0
