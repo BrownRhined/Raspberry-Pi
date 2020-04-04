60600255ee1473c37f73590be0335148b4d78c83
BrownRhined <a728728728@gmail.com>
2020-04-05 04:01:49 +0800

    Update capturePhoto.sh

--- capturePhoto.sh
+++ capturePhoto.sh
@@ -10,6 +10,5 @@ mkdir -p /home/pi/eoa-ai-image/$FolderName
 # 透過fswebcam截圖，先skip 60張圖讓webcam先自動對焦完成
 # 將banner設定成透明，並加入時間戳記與調整字型
 fswebcam -r 1280x720 -S 20 --banner-colour '#FF000000' --line-colour '#FF000000' --timestamp '%Y-%m-%d %H:%M' --font 'sans:32' /home/pi/eoa-ai-image/$FolderName/$DATE.jpg
-
 # return 0 to system
 exit 0
