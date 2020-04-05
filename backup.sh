e1782671a03b298a89a53134b5564f17de5b572d
BrownRhined <a728728728@gmail.com>
2020-04-05 08:25:50 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,16 +3,16 @@ cd /home/pi
 HOST='192.168.0.1'
 USER='localadmin'
 PASSWD='PASSWORD'
-DATE=$(date +%Y%m%d)
+FolderName=$(date +%Y%m%d)
+DATE=$(date +%Y%m%d%H%M)
 
-sudo tar -zcvf eoa-ai-image-$(date +%Y%m%d).tar.gz eoa-ai-image
+sudo tar -zcvf eoa-ai-image-$FolderName.tar.gz $FolderName
 
 ftp -n -v $HOST << EOT
 ascii
 user $USER $PASSWD
 prompt
-put eoa-ai-image-$(date +%Y%m%d).tar.gz
+put eoa-ai-image-$FolderName.tar.gz
 bye
 EOT
-
-
+rm  eoa-ai-image-$FolderName.tar.gz
